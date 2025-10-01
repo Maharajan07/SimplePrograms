e4f50d55bcda13bff64bdf13ef9ac6f6ff416984
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-10-01 15:44:30 +0530

    Number ha Equal Digit Count & Digit Value

--- lastdigit.java
+++ lastdigit.java
@@ -1,15 +1,20 @@
 import java.util.*;
 
 public class lastdigit{
+
+    static int lastDigit(int base, int power) {
+        int num = (int) Math.pow(base, power);
+        int res = num % 10;
+        return res;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number for Base: ");
         int base = sc.nextInt();
         System.out.print("Enter a number for Power: ");
         int power = sc.nextInt();
-        double num = Math.pow(base, power);
-        int result = (int) (num%10);
-        System.out.println("The last digit of given number is: " +result);
+        
+        System.out.println("The last digit of given number is: " + lastDigit(base, power));
         sc.close();
 
     }
