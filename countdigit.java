c3335cb35e33a5c38d61d8aef7a02c45afb29194
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-10-01 14:26:10 +0530

    Counting digits that divides the number eventually

--- countdigit.java
+++ countdigit.java
@@ -1,20 +1,37 @@
 import java.util.*;
 
 public class countdigit {
+
+    static int countDigit(int num) {
+        int n = num;
+        int count = 0;
+        while (n != 0) {
+            int digit = n % 10;     // gets last digit
+            if (digit != 0 && num % digit == 0) {
+                count++;
+            }
+            n = n/10;       // removes last digit
+        }
+        return count;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.print("Enter a Number: ");
         int num = sc.nextInt();
-        int count = 0;
-        String numStr = Integer.toString(num);
-        for (int i=numStr.length()-1; i>=0; i--){
-            int digit = numStr.charAt(i) - '0';
+        // int count = 0;
+
+        /* // method -1
+        String numStr = Integer.toString(num);      // converts integer to string
+        for (int i=0; i<numStr.length(); i++){
+            int digit = numStr.charAt(i) - '0';     // gets actual value
             if( digit != 0 && num%digit == 0){
                 count++;
             }
-        }
+        } 
+        System.out.println("The count of the digit that divides the given number eventually is: " + count);
+        */
 
-        System.out.println("The count of the digit that divides the given number eventually is: "+count);
+        System.out.println("The count of the digit that divides the given number eventually is: " + countDigit(num));
         sc.close();
 
     }
